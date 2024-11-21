c8ec55e55532b4cf74988fb2149d12910f5105a2
ssnlabs <137744428+ssnlabs@users.noreply.github.com>
2024-11-21 18:24:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "Hello, this is a simple Flask app!"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+app=Flask(__name__)
+@app.route('/')
+def run():
+    return "vicky punda"
+app.run('0.0.0.0',5000)
