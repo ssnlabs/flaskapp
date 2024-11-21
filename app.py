747fdd8dc0b2bf40bd4f99fae0030034d5625636
ssnlabs <137744428+ssnlabs@users.noreply.github.com>
2024-11-21 18:18:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 from flask import Flask
 
-# Create the Flask app
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, this is a basic Flask app!"
+    return "Hello, this is a simple Flask app!"
 
-# Run the app
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8080)
